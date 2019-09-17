672d32e589d26e44363619f0ab0a78a061e8b51c
Marcel Guzik <mguzik555@gmail.com>
2019-09-17 21:28:03 +0200

    ixed memory leak, added changing lights' mode, and handling interrupt signal

--- main.c
+++ main.c
@@ -7,21 +7,37 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdint.h>
+#include <signal.h>
 
 #include <X11/X.h>
 #include <X11/Xlib.h>
 
+int fd;
+
+void int_handler(int sig) {
+    write(fd, "\x04\x00", 2);
+    close(fd);
+
+    exit(0);
+}
+
 int main(int argc, char** argv) {
     // serial port
-    int fd = open("/dev/ttyUSB0", O_RDWR | O_SYNC | O_NOCTTY);
+    fd = open("/dev/ttyUSB0", O_RDWR | O_SYNC | O_NOCTTY);
+    if(fd < 0) {
+        fprintf(stderr, "fuck\n");
+        return 1;
+    }
+
+    signal(SIGINT, int_handler);
 
     struct termios tty;
     struct termios tty_old;
     memset(&tty, 0, sizeof tty);
     tty_old = tty;
 
-    cfsetispeed(&tty, (speed_t)B9600);
-    cfsetospeed(&tty, (speed_t)B9600);
+    cfsetispeed(&tty, (speed_t)B115200);
+    cfsetospeed(&tty, (speed_t)B115200);
 
     tty.c_cflag     &=  ~PARENB;            // Make 8n1
     tty.c_cflag     &=  ~CSTOPB;
@@ -36,12 +52,6 @@ int main(int argc, char** argv) {
     tcflush(fd, TCIFLUSH);
     tcsetattr(fd, TCSANOW, &tty);
 
-    if(fd < 0) {
-        fprintf(stderr, "fuck");
-        return 1;
-    }
-
-
 
     Display* display = XOpenDisplay((char *) NULL);
 
@@ -49,9 +59,12 @@ int main(int argc, char** argv) {
         fprintf(stderr, "an error occured");
         return 1;
     }
-
     Window rootWindow = XRootWindow(display, XDefaultScreen(display));
     unsigned long counter = 0;
+
+    write(fd, "\x04\xff", 2);
+
+
     while(1) {
         XImage* image = XGetImage(display, rootWindow, 0, 0, 1920, 1080, AllPlanes,ZPixmap);
         int status = XInitImage(image);
@@ -65,15 +78,11 @@ int main(int argc, char** argv) {
         printf("[%lu] %x\n", counter, a);
         printf("[%lu] %x %x %x\n", counter, red, green, blue);
 
-        uint8_t payload[] = {0x01, red, green, blue};
+        uint8_t payload[] = {0x03, red, green, blue};
         write(fd, payload, 4);
 
-        sleep(1);
-
-        uint8_t colours[3];
-        read(fd, &colours, 3);
-        printf("[%lu] READ: %x %x %x\n", counter, colours[0], colours[1], colours[2]);
-
+        XDestroyImage(image);
+        usleep(200000);
         counter++;
     }
 
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gcc main.c -Wall -lX11 && ./a.out
+gcc main.c -Wall -lX11 -ggdb3 && ./a.out
